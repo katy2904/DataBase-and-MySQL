5496776f4aec1ea2459f3c393190a195456a21fa
Katy <wc-katy@yandex.ru>
2023-04-14 00:01:15 +0700

    homework 1 is done

--- sem_1_homework.sql
+++ sem_1_homework.sql
@@ -1,7 +1,10 @@
+# создаём новую базу данных
 CREATE DATABASE My_homeworks;
 
+# переходим к использованию созданной базы (делаем ее активной)
 USE My_homeworks;
 
+# создаём в новой базе таблицу, задаём список полей и их тип данных
 CREATE TABLE mobile_phones
 (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -11,6 +14,7 @@ CREATE TABLE mobile_phones
     price DECIMAL
 );
 
+# заполняем таблицу данными
 INSERT INTO mobile_phones
 (product_name, manufacturer, product_count, price)
 VALUES
@@ -20,28 +24,27 @@ VALUES
 ('Galaxy S8', 'Samsung', 1, 41000),
 ('P20 Pro', 'Huawei', 5, 36000);
 
+# выводим название, производителя и цену товаров, количество которых превышает 2
 SELECT product_name, manufacturer, price
 FROM mobile_phones
 WHERE product_count > 2;
 
+# выводим весь ассортимент товаров марки "Samsung"
 SELECT * FROM mobile_phones
 WHERE manufacturer = 'Samsung';
 
+# выводим товары, в которых есть упоминание "iPhone"
 SELECT * FROM mobile_phones
 WHERE product_name LIKE '%iPhone%';
 
+# выводим товары, в которых есть упоминание "Samsung"
 SELECT * FROM mobile_phones
 WHERE product_name LIKE '%Samsung%' OR manufacturer LIKE '%Samsung%';
 
+# выводим товары, в котовых есть цифры
 SELECT * FROM mobile_phones
 WHERE product_name RLIKE '[0-9]' OR manufacturer RLIKE '[0-9]';
 
-
+# выводим товары, в котовых есть цифра 8
 SELECT * FROM mobile_phones
 WHERE product_name RLIKE '[8]' OR manufacturer RLIKE '[8]';
-
-
-
-git remote add origin https://github.com/katy2904/DataBase-and-MySQL.git
-git branch -M main
-git push -u origin main
